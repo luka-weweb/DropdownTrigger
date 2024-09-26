9c4843a65b2b2796bb547d850b663dcd14e61a61
luka-weweb <luka@weweb.io>
2024-09-26 14:33:06 +0200

    update

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -4,14 +4,10 @@ export default {
     content: { type: Object, required: true },
     wwFrontState: { type: Object, required: true },
     wwEditorState: { type: Object, required: true },
-    randomProp: { type: String, required: false },
   },
 };
 </script>
 
 <template>
-  <div>
-    <span> {{ randomProp }}</span>
-    <wwLayout path="slot" />
-  </div>
+  <wwLayout path="slot" />
 </template>
